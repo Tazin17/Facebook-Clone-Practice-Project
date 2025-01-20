77edc743a684a293d13ed2007738fab61ea86bcb
Nusrat <nusrattazin9081@gmail.com>
2025-01-20 15:43:54 +0600

    commit

--- CUETbook-A-MERN-Stack-Social-Media-Website-main/frontend/src/app/components/Header.jsx
+++ CUETbook-A-MERN-Stack-Social-Media-Website-main/frontend/src/app/components/Header.jsx
@@ -46,13 +46,13 @@ const Header = () => {
   if (!mounted) return null;
 
   return (
-    <header className="bg-white dark:bg-[rgb(36,37,38)] text-foreground shadow-md h-20 fixed top-0 left-0 right-0 z-50 p-2">
+    <header className="bg-white dark:bg-[rgb(36,37,38)] text-foreground shadow-md h-15 fixed top-0 left-0 right-0 z-50 p-2">
       <div className="mx-auto flex justify-between items-center p-2">
         <div className="flex items-center gap-2 md:gap-4">
           <Image
-            src="/Images/CUETbook.png"
-            width={60}
-            height={38}
+            src="/Images/Facebook-Logo.png"
+            width={74}
+            height={44}
             alt="cuetbook-logo"
           />
           <div className="relative">
