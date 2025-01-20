661f8926265f57d123aba94b57971422d139118d
Nusrat <nusrattazin9081@gmail.com>
2025-01-20 15:47:29 +0600

    commit

--- CUETbook-A-MERN-Stack-Social-Media-Website-main/backend/utils/generateToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-const generateToken = (user)=>{
-    return jwt.sign({userId:user?._id,email:user.email}, process.env.IWT_SECRET,{expiresIn:"90d"});
-}
-
-module.exports={generateToken}
--- CUETbook-A-MERN-Stack-Social-Media-Website-main/backend/utils/responseHandler.js
+++
@@ -1,9 +0,0 @@
-const response = (res,stateCode, message,data=null)=>{
-    const responseObject={
-        status:stateCode<400?'success':'error',
-        message,
-        data
-    }
-    return res.status(stateCode).json(responseObject)
-}
-module.exports=response;
