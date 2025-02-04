b9ef16b08620c707295f841ba4790b10968a8de5
Nusrat <nusrattazin9081@gmail.com>
2025-02-04 13:37:58 +0600

    commit

--- CUETbook-A-MERN-Stack-Social-Media-Website-main/backend/controllers/postController.js
+++ CUETbook-A-MERN-Stack-Social-Media-Website-main/backend/controllers/postController.js
@@ -1,5 +1,6 @@
 const { uploadFileToCloudinary } = require("../config/cloudinary");
 const Post = require("../model/Post");
+const Story = require("../model/story");
 const response = require("../utils/responseHandler");
 
 const createPost = async (req, res) => {
@@ -35,6 +36,60 @@ const createPost = async (req, res) => {
   } catch (error) {
     console.log("error creating post", error);
     return response(res, 500, "Internal server error", error.message);
+  }
+};
+
+//create story
+
+const createStory = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    // console.log('this is my auth user id',userId);
+
+    const file = req.file;
+
+    if (!file) {
+      return response(res, 400, "File is required to create a story");
+    }
+    let mediaUrl = null;
+    let mediaType = null;
+
+    if (file) {
+      const uploadResult = await uploadFileToCloudinary(file);
+      // console.log('upload file', uploadResult)
+      mediaUrl = uploadResult?.secure_url;
+      mediaType = file.mimetype.startsWith("video") ? "video" : "image";
+    }
+
+    //create a new story
+    const newStory = await new Story({
+      user: userId,
+
+      mediaUrl,
+      mediaType,
+    });
+
+    await newStory.save();
+    return response(res, 201, "Story created successfully", newStory);
+  } catch (error) {
+    console.log("error creating story", error);
+    return response(res, 500, "Internal server error", error.message);
+  }
+};
+
+//get all story
+const getAllStory = async (req, res) => {
+  try {
+    const story = await Story.find()
+      .sort({
+        createdAt: -1,
+      })
+      .populate("user", "_id username profilePicture email");
+
+    return response(res, 201, "Get all stories successfully ", story);
+  } catch (error) {
+    console.log("error getting stories", error);
+    return response(res, 500, "internal server error", error.message);
   }
 };
 
@@ -74,51 +129,102 @@ const getPostByUserId = async (req, res) => {
         path: "comments.user",
         select: "username, profilePicture",
       });
-      return response(res,201,'Get user posts successfully',posts)
+    return response(res, 201, "Get user posts successfully", posts);
   } catch (error) {
     console.log("error getting posts", error);
     return response(res, 500, "internal server error", error.message);
   }
 };
 
-
 //like post api
-const likePost = async(req,res) =>{
-    const {postId} = req.params;
-    const userId = req.user.userId;
-    try{
-        const post = await Post.findById(postId)
-        if(!post){
-            return response(res,404, 'Post not found')
-        }
-        const hasliked = post.likes.includes(userId)
-        if(hasliked){
-            post.likes = post.likes.filter(id => id.toString() !== userId.toString())
-            post.likeCount = Math.max(0, post.likeCount - 1); //ensure likeCount does not become negative
-
-        }else{
-          post.likes.push(userId)
-          post.likeCount += 1
-        }
-
-//save the likes in the updated post
-const updatedpost = await post.save()
-return response(res,201, hasliked ? "Post unliked successfully" : "Post liked successfully", updatedpost)
-
-
-    }catch(error){
-        console.log("error getting posts", error);
-        return response(res, 500, "internal server error", error.message);
+const likePost = async (req, res) => {
+  const { postId } = req.params;
+  const userId = req.user.userId;
+  try {
+    const post = await Post.findById(postId);
+    if (!post) {
+      return response(res, 404, "Post not found");
     }
-}
+    const hasliked = post.likes.includes(userId);
+    if (hasliked) {
+      post.likes = post.likes.filter(
+        (id) => id.toString() !== userId.toString()
+      );
+      post.likeCount = Math.max(0, post.likeCount - 1); //ensure likeCount does not become negative
+    } else {
+      post.likes.push(userId);
+      post.likeCount += 1;
+    }
 
+    //save the likes in the updated post
+    const updatedpost = await post.save();
+    return response(
+      res,
+      201,
+      hasliked ? "Post unliked successfully" : "Post liked successfully",
+      updatedpost
+    );
+  } catch (error) {
+    console.log(error);
+    return response(res, 500, "internal server error", error.message);
+  }
+};
 
+//post comments by user
+const addCommentToPost = async (req, res) => {
+  const { postId } = req.params;
+  const userId = req.user.userId;
+  const { text } = req.body;
 
+  try {
+    const post = await Post.findById(postId);
+    if (!post) {
+      return response(res, 404, "Post not found");
+    }
 
+    post.comments.push({ user: userId, text });
+    post.commentCount += 1;
+
+    //save the post with new comments
+    await post.save();
+    return response(res, 201, "comments addede successfully", post);
+  } catch (error) {
+    console.log(error);
+    return response(res, 500, "internal server error", error.message);
+  }
+};
+
+//share on post by user
+const sharePost = async (req, res) => {
+  const { postId } = req.params;
+  const userId = req.user.userId;
+  try {
+    const post = await Post.findById(postId);
+    if (!post) {
+      return response(res, 404, "Post not found");
+    }
+    const hasUserShared = post.share.includes(userId);
+    if (!hasUserShared) {
+      post.share.push(userId);
+    }
+    post.shareCount += 1;
+
+    //save the share in the updated post
+    await post.save();
+    return response(res, 201, "post shared successfully", post);
+  } catch (error) {
+    console.log(error);
+    return response(res, 500, "internal server error", error.message);
+  }
+};
 
 module.exports = {
   createPost,
   getAllPosts,
   getPostByUserId,
-  likePost
+  likePost,
+  addCommentToPost,
+  sharePost,
+  createStory,
+  getAllStory
 };
--- CUETbook-A-MERN-Stack-Social-Media-Website-main/backend/routes/postRoute.js
+++ CUETbook-A-MERN-Stack-Social-Media-Website-main/backend/routes/postRoute.js
@@ -1,22 +1,53 @@
-const express = require ('express');
-const authMiddleware = require('../middleware/authMiddleware');
-const { multerMiddleware } = require('../config/cloudinary');
-const { createPost, getAllPosts, getPostByUserId, likePost } = require('../controllers/postController');
+const express = require("express");
+const authMiddleware = require("../middleware/authMiddleware");
+const { multerMiddleware } = require("../config/cloudinary");
+const {
+  createPost,
+  getAllPosts,
+  getPostByUserId,
+  likePost,
+  sharePost,
+  addCommentToPost,
+  createStory,
+  getAllStory
+} = require("../controllers/postController");
 
-const router = express.Router()
+const router = express.Router();
 
-//create posts
-router.post('/posts',authMiddleware,multerMiddleware.single('media'),createPost)
+//create post
+router.post(
+  "/posts",
+  authMiddleware,
+  multerMiddleware.single("media"),
+  createPost
+);
 
 //get all posts
-router.get('/posts',authMiddleware,getAllPosts)
+router.get("/posts", authMiddleware, getAllPosts);
 
 //get post by userid
-router.get('/posts/user/:userId',authMiddleware,getPostByUserId)
+router.get("/posts/user/:userId", authMiddleware, getPostByUserId);
 
 //user liked post route
-router.post('/posts/likes/:postId',authMiddleware,likePost)
+router.post("/posts/likes/:postId", authMiddleware, likePost);
 
+//user comments post route
+router.post("/posts/comments/:postId", authMiddleware, addCommentToPost);
+
+//user shared post route
+router.post("/posts/share/:postId", authMiddleware, sharePost);
+
+
+//create story
+router.post(
+  "/story",
+  authMiddleware,
+  multerMiddleware.single("media"),
+  createStory
+);
+
+//get all stories
+router.get("/story", authMiddleware, getAllStory);
 
 
 module.exports = router;
